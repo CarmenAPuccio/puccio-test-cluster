ff5b1bcba4a2056fb295e59840d5cfa64c88c3cc
Carmen Puccio <CarmenAPuccio@gmail.com>
2022-03-07 14:31:26 -0500

    Trying a role

--- platform-team/index.ts
+++ platform-team/index.ts
@@ -1,13 +1,10 @@
-import { ArnPrincipal } from "@aws-cdk/aws-iam";
 import { PlatformTeam } from '@aws-quickstart/ssp-amazon-eks';
 
 export class TeamPlatform extends PlatformTeam {
     constructor(accountID: string) {
         super({
             name: "team-platform",
-            users: [
-                new ArnPrincipal('arn:aws:iam::' + accountID +':role/Admin')
-            ]
+            userRoleArn: 'arn:aws:iam::' + accountID +':role/Admin'
         })
     }
 }
