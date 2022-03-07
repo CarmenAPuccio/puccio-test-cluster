ae13a31f9d2b491eca4d0b64bf2e2035aed6b87c
Carmen Puccio <CarmenAPuccio@gmail.com>
2022-03-07 16:11:14 -0500

    Take 3

--- platform-team/index.ts
+++ platform-team/index.ts
@@ -1,10 +1,12 @@
+import { ArnPrincipal } from "@aws-cdk/aws-iam";
+
 import { PlatformTeam } from '@aws-quickstart/ssp-amazon-eks';
 
 export class TeamPlatform extends PlatformTeam {
     constructor(accountID: string) {
         super({
             name: "team-platform",
-            userRoleArn: `arn:aws:iam::${accountID}:role/Admin`
+            users: [new ArnPrincipal(`arn:aws:iam::${accountID}:role/Admin`)]
         })
     }
 }
