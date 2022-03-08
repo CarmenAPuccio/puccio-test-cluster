20301e08de40abb8e51f905b280a685b4454dfe4
Carmen Puccio <CarmenAPuccio@gmail.com>
2022-03-08 13:13:27 -0500

    Test

--- platform-team/index.ts
+++ platform-team/index.ts
@@ -6,7 +6,8 @@ export class TeamPlatform extends PlatformTeam {
     constructor(accountID: string) {
         super({
             name: "platform",
-            userRoleArn: `arn:aws:iam::${accountID}:role/Admin`
+            userRoleArn: `arn:aws:iam::${accountID}:role/Admin`,
+            users: [new ArnPrincipal(`arn:aws:sts::${accountID}:assumed-role/Admin/puccio-Isengard`)]
         });
     }
 }
