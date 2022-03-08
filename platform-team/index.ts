b70dcb43d34f3cdbcf1a7194e692cdd6e45b6911
Carmen Puccio <CarmenAPuccio@gmail.com>
2022-03-08 09:16:26 -0500

    trying assumed role

--- platform-team/index.ts
+++ platform-team/index.ts
@@ -6,7 +6,9 @@ export class TeamPlatform extends PlatformTeam {
     constructor(accountID: string) {
         super({
             name: "platform",
-            users: [new ArnPrincipal(`arn:aws:iam::${accountID}:role/Admin`)]
+            users: [
+                new ArnPrincipal(`arn:aws:iam::${accountID}:role/Admin`),
+                new ArnPrincipal(`arn:aws:sts::${accountID}:assumed-role/Admin/puccio-Isengard`)]
         })
     }
 }
