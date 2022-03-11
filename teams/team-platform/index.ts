8b658dd2baee6d591a79f3eb77193444fa1c3011
Carmen Puccio <CarmenAPuccio@gmail.com>
2022-03-11 11:11:05 -0500

    Adding ResourceQuotoa

--- teams/team-platform/index.ts
+++ teams/team-platform/index.ts
@@ -4,7 +4,7 @@ import { PlatformTeam } from '@aws-quickstart/ssp-amazon-eks';
 export class TeamPlatform extends PlatformTeam {
     constructor(accountID: string) {
         super({
-            name: "team-platform",
+            name: "platform",
             userRoleArn: `arn:aws:iam::${accountID}:role/Admin`,
             // The below is currently a bug. When userRoleArn is specified, there needs to be a at least one user.
             users: [new ArnPrincipal(`arn:aws:sts::${accountID}:assumed-role/Admin/puccio-Isengard`)]
--- teams/team-puccio/index.ts
+++ teams/team-puccio/index.ts
@@ -1,10 +1,13 @@
 import * as iam from '@aws-cdk/aws-iam';
 import { ApplicationTeam } from '@aws-quickstart/ssp-amazon-eks';
 
+const puccioManifestDir = './manifests/'
+
 export class TeamPuccio extends ApplicationTeam {
     constructor(accountID: string) {
         super({
-            name: "puccio"
+            name: "puccio",
+            teamManifestDir: puccioManifestDir
         });
     }
 }
